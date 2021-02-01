0c8c72db7a5007e1f33e542b36ec4d2c3cd2a965
Andy <admazzola@gmail.com>
2021-02-01 10:57:05 -0500

    iinit

---
+++ src/router.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+//import Router from './hash-router.vue'
+
+import Home from './views/Home.vue'
+
+import NotFound from './views/NotFound.vue'
+
+Vue.use(Router)
+
+export default new Router({
+//  mode: 'history',
+//  base: process.env.PUBLIC_URL,
+  routes: [
+
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+
+    {
+      path: '/#',
+      name: 'home',
+      component: Home
+    },
+
+   
+    {
+      path: '/*',
+      component: NotFound
+    },
+  ]
+})
---
+++ src/views/components/Footer.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="section footer">
+    <div class="container-3 w-container">
+      <div class="w-row">
+        <div class="column-5 w-col w-col-4 w-col-small-4">
+          <h3 class="heading-3 footer">Documentation</h3>
+
+
+          <a href="https://github.com/0xbitcoin/white-paper" target="_blank" class="footer-link">White Paper</a>
+          <a href="https://etherscan.io/address/0xb6ed7644c69416d67b522e20bc294a9a9b405b31#code" target="_blank" class="footer-link">Contract Code</a>
+          <a href="https://github.com/EthereumCommonwealth/Auditing/issues/102" target="_blank" class="footer-link">Smart Contract Audit</a>
+          <a href="https://0xbtc.info/" target="_blank" class="footer-link">0xBTC Block Explorer</a></div>
+        <div class="column-6 w-col w-col-4 w-col-small-4">
+          <h3 class="heading-3 footer">Community &amp; Social</h3>
+          <a href="https://github.com/0xbitcoin" target="_blank" class="footer-link">0xBTC Github</a>
+
+          <a href="https://discordapp.com/invite/JGEqqmS" target="_blank" class="footer-link">Discord</a>
+          <a href="https://twitter.com/0xbitcoin_erc20" target="_blank" class="footer-link">Twitter</a>
+          <a href="https://www.reddit.com/r/0xbitcoin/" target="_blank" class="footer-link">Reddit</a></div>
+        <div class="column-7 w-col w-col-4 w-col-small-4 flex flex-col h-full">
+
+          <div class="flex-grow h-32">
+          </div>
+
+
+          <div class="text-yellow-400">
+           <span class="text-gray-400"> Powered by </span> <a href="https://zapper.fi" target="_blank" class="    ">Zapper.fi</a>
+         </div>
+
+         </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+
+export default {
+  name: 'Footer',
+  props: [],
+  data() {
+    return {
+
+    }
+  },
+  created(){
+
+  },
+  methods: {
+
+  }
+}
+</script>
