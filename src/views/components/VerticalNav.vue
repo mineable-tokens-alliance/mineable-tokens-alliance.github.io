0e8664dc9332821e0139fc60f0715dfcc3609265
Andy <admazzola@gmail.com>
2021-02-01 17:03:45 -0500

    fix color

--- src/views/components/VerticalNav.vue
+++ src/views/components/VerticalNav.vue
@@ -1,13 +1,13 @@
 <template>
   <div   class="navbar h-nav">
-    <div class=" grid grid-flow-col md:grid-flow-row  ">
+    <div class=" grid grid-flow-col md:grid-flow-row text-gray-900 ">
 
       <div
 
       v-for="name in buttonNames"
       @click="onClick(name)"
-      class="font-sans bg-gray-200 border-2 border-gray-500 hover:bg-gray-800 hover:text-gray-200 cursor-pointer capitalize text-gray-900 text-center p-2 m-2"
-      :class="{'bg-green-500 text-gray-200': (name == activeSection) }"
+      class="font-sans bg-gray-200 border-2 border-gray-500 hover:bg-gray-800 hover:text-gray-200 cursor-pointer capitalize  text-center p-2 m-2"
+      :class="{'bg-green-500': (name == activeSection) , 'text-gray-200': (name == activeSection)}"
       >
        {{name}}
      </div>
